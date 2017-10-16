6fda7a32ce770fa067e09662608588d16f91a934
Natalie Lang <natalielang@Natalies-MacBook-Pro.local>
2017-10-16 16:02:52 +0100

    Modified submit_job.sh: runs first CNN

--- Lab_1_intro/submit_job.sh
+++ Lab_1_intro/submit_job.sh
@@ -7,9 +7,10 @@
 #SBATCH -o hostname_%j.out # File to which STDOUT will be written                                                                                    
 #SBATCH -e hostname_%j.err # File to which STDERR will be written                                                                                    
 #SBATCH --mail-type=ALL # Type of email notification- BEGIN,END,FAIL,ALL                                                                             
-#SBATCH --mail-user=<your_uob_mail_here>@bristol.ac.uk # Email to which notifications will be sent                                                                
+#SBATCH --mail-user=nl14833@my.bristol.ac.uk # Email to which notifications will be sent                                                                
 
 module add libs/tensorflow/1.2
 
-srun python testing_tensorflow.py
+srun python python simple_train_cifar.py &
+ tensorboard --logdir=logs/ --port=17505
 wait
